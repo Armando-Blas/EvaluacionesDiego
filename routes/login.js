bcec0c84607883b19c4cd152cd958b69224de394
Armando-Blas <joseblas@seekop.com>
2025-01-21 01:22:15 -0600

    first Commit

---
+++ routes/login.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const pool = require('../config/database');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'EVALUACIONES DOCENTES' });
+});
+router.post('/', (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'admin' && password === '12345') {
+        req.session.user = username;
+        return res.redirect('/home');
+    } else {
+        res.status(401).send('Credenciales inválidas.');
+    }
+});
+module.exports = router;
